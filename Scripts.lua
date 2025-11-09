d678060742fbdb1b6a68d1e6a9a8cc937f0b174c
ShaanDaBest <shaan.mirch@gmail.com>
2025-11-08 19:35:47 -0800

    scripts

--- Scripts.lua
+++ Scripts.lua
@@ -1,60 +1,11 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = OrionLib:MakeWindow({Name = "AeroHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local Window = Rayfield:CreateWindow({
-   Name = "Aero",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Aero Interface",
-   LoadingSubtitle = "by Shaan",
-   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "q", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "AeroKey",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "AeroKeySystem", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"AeoTester","AeroDev","AeroOwner"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+local playerTab = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
 })
 
-local PlayerTab = Window:CreateTab("Player", 4483362458) -- Title, Image
-
-local Section = PlayerTab:CreateSection("Movement")
-
-local Slider = PlayerTab:CreateSlider({
-   Name = "Walk Speed",
-   Range = {0, 100},
-   Increment = 1,
-   Suffix = "Speed",
-   CurrentValue = 10,
-   Flag = "WalkSpeedSlider",
-   Callback = function(Value)
-      local player = game.Players.LocalPlayer
-      local character = player.Character or player.CharacterAdded:Wait()
-      local humanoid = character:WaitForChild("Humanoid")
-
-      humanoid.WalkSpeed = Value
-   end,
+local Section = MovementTab:AddSection({
+	Name = "Movement"
 })
-
-
